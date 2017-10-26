769ffa91417d51d69533bfb5c48219fac1a43040
Leonid Bobylev <leonbobster@gmail.com>
2017-10-26 21:37:11 +0200

    PeakFinder::run takes file name arg

--- app/src/App.java
+++ app/src/App.java
@@ -1,14 +1,9 @@
-import java.io.FileInputStream;
-import java.util.Scanner;
 import PeakFinder.*;
 
 class App {
     public static void main(String[] args) {
-        try (
-            FileInputStream is = new FileInputStream(args[0]);
-            Scanner sc = new Scanner(is, "UTF-8");
-        ) {
-            int[] peak = PeakFinder.run(sc);
+        try {
+            int[] peak = PeakFinder.run(args[0]);
             System.out.format(
                 "The peak for this call log is %d simultaneous calls, that occured between %d and %d.\n", 
                 peak[0], 
--- app/src/PeakFinder/PeakFinder.java
+++ app/src/PeakFinder/PeakFinder.java
@@ -1,5 +1,7 @@
 package PeakFinder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.List;
 import java.util.stream.Stream;
@@ -7,38 +9,43 @@ import java.util.stream.Collectors;
 import PeakFinder.Exception.*;
 
 public class PeakFinder {
-    public static int[] run(Scanner data) {
+    public static int[] run(String file) throws IOException {
         int peakStartTime = 0;
         int peakEndTime = 0;
         int peakNumOfCalls = 0;
         int[] tmp = new int[]{0, 0, 0};
 
-        if (data.hasNextLine()) {
-            List<Integer> fl = parseLine(data.nextLine());
-            peakStartTime = fl.get(0);
-            peakEndTime = fl.get(1);
-            peakNumOfCalls = 1;
-
-            while (data.hasNextLine()) {
-                List<Integer> n = parseLine(data.nextLine());
-                int st = n.get(0);
-                int et = n.get(1);
+        try (
+            FileInputStream is = new FileInputStream(file);
+            Scanner data = new Scanner(is, "UTF-8");
+        ) {
+            if (data.hasNextLine()) {
+                List<Integer> fl = parseLine(data.nextLine());
+                peakStartTime = fl.get(0);
+                peakEndTime = fl.get(1);
+                peakNumOfCalls = 1;
     
-                if (st < peakEndTime) {
-                    peakNumOfCalls += 1;
-                    peakStartTime = st;
-                    peakEndTime = et < peakEndTime ? et : peakEndTime;
-                } else {
-                    if (tmp[0] < peakNumOfCalls) {
-                        tmp = new int[]{
-                            peakNumOfCalls,
-                            peakStartTime,
-                            peakEndTime
-                        };
+                while (data.hasNextLine()) {
+                    List<Integer> n = parseLine(data.nextLine());
+                    int st = n.get(0);
+                    int et = n.get(1);
+        
+                    if (st < peakEndTime) {
+                        peakNumOfCalls += 1;
+                        peakStartTime = st;
+                        peakEndTime = et < peakEndTime ? et : peakEndTime;
+                    } else {
+                        if (tmp[0] < peakNumOfCalls) {
+                            tmp = new int[]{
+                                peakNumOfCalls,
+                                peakStartTime,
+                                peakEndTime
+                            };
+                        }
+                        peakNumOfCalls = 1;
+                        peakStartTime = st;
+                        peakEndTime = et;
                     }
-                    peakNumOfCalls = 1;
-                    peakStartTime = st;
-                    peakEndTime = et;
                 }
             }
         }
