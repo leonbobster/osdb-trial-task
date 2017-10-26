dae7fc5b3bc48f3952e6f74a9358a6326c234b9e
Leonid Bobylev <leonbobster@gmail.com>
2017-10-26 19:56:36 +0200

    use file scanner for less memory consumtion

--- app/src/PeakFinder/PeakFinder.java
+++ app/src/PeakFinder/PeakFinder.java
@@ -1,47 +1,62 @@
 package PeakFinder;
 
-import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import PeakFinder.Exception.*;
 
 public class PeakFinder {
-    public static int[] run(ArrayList<Integer> data) {
-        if (data.size() < 2 || data.size() % 2 != 0) {
-            throw new MalformedData();
-        }
-
-        int peakStartTime = data.get(0);
-        int peakEndTime = data.get(1);
-        int peakNumOfCalls = 1;
+    public static int[] run(Scanner data) {
+        int peakStartTime = 0;
+        int peakEndTime = 0;
+        int peakNumOfCalls = 0;
         int[] tmp = new int[]{0, 0, 0};
 
-        for (int i = 2; i < data.size(); i += 2) {
-            int st = data.get(i);
-            int et = data.get(i + 1);
+        if (data.hasNextLine()) {
+            List<Integer> fl = parseLine(data.nextLine());
+            peakStartTime = fl.get(0);
+            peakEndTime = fl.get(1);
+            peakNumOfCalls = 1;
 
-            if (st < data.get(i - 2)) {
-                throw new MalformedData();
-            }
-
-            if (st < peakEndTime) {
-                peakNumOfCalls += 1;
-                peakStartTime = st;
-                peakEndTime = et < peakEndTime ? et : peakEndTime;
-            } else {
-                if (tmp[0] < peakNumOfCalls) {
-                    tmp = new int[]{
-                        peakNumOfCalls,
-                        peakStartTime,
-                        peakEndTime
-                    };
+            while (data.hasNextLine()) {
+                List<Integer> n = parseLine(data.nextLine());
+                int st = n.get(0);
+                int et = n.get(1);
+    
+                if (st < peakEndTime) {
+                    peakNumOfCalls += 1;
+                    peakStartTime = st;
+                    peakEndTime = et < peakEndTime ? et : peakEndTime;
+                } else {
+                    if (tmp[0] < peakNumOfCalls) {
+                        tmp = new int[]{
+                            peakNumOfCalls,
+                            peakStartTime,
+                            peakEndTime
+                        };
+                    }
+                    peakNumOfCalls = 1;
+                    peakStartTime = st;
+                    peakEndTime = et;
                 }
-                peakNumOfCalls = 1;
-                peakStartTime = st;
-                peakEndTime = et;
             }
         }
 
         return tmp[0] < peakNumOfCalls
             ? new int[] {peakNumOfCalls, peakStartTime, peakEndTime}
             : tmp;
+    }
+
+    private static List<Integer> parseLine(String line) {
+        List<Integer> nums = Stream.of(line.split(":"))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+
+        if (nums.size() != 2) {
+            throw new MalformedData();
+        }
+
+        return nums;
     }
 }
